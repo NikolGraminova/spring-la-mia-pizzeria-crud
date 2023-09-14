440b9ffa6228ec9b412aca19a986bbb962b50cdb
NikolGraminova <nbackup619@gmail.com>
2023-09-14 14:51:39 +0200

    Completed main program

--- src/main/java/org/web/springlamiapizzeriacrud/controller/IndexController.java
+++ src/main/java/org/web/springlamiapizzeriacrud/controller/IndexController.java
@@ -2,19 +2,25 @@ package org.web.springlamiapizzeriacrud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.web.springlamiapizzeriacrud.model.Pizza;
 import org.web.springlamiapizzeriacrud.repository.PizzaRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
-public class PizzaController {
+public class IndexController {
 
     @Autowired
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index() {
-        return "pizzas";
+    public String index(Model model) {
+        List<Pizza> pizzaList = pizzaRepository.findAll();
+        model.addAttribute("pizzas", pizzaList);
+        return "pizza-list";
     }
 }
