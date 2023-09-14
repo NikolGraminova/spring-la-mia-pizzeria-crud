15600c89726aa62056c21f2395e3e2861c4ebd72
NikolGraminova <nbackup619@gmail.com>
2023-09-14 16:00:27 +0200

    Bootstrap customization and bonus images added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO pizzas (name, description, price) VALUES('Margherita', 'Pomodoro e mozzarella', 5.99);
 INSERT INTO pizzas (name, description, price) VALUES('Diavola', 'Salame piccante', 7.50);
-INSERT INTO pizzas (name, description, price) VALUES('Quattro Formaggi', 'Combinazione di quattro formaggi diversi', 6.99);
+INSERT INTO pizzas (name, description, price) VALUES('Cotto e mozzarella', 'Prosciutto cotto e mozzarella', 6.99);
